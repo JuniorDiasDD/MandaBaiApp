fbe0cc8e984f7e8e81cc682f35c13a1da8169c90
rossana <mendesro03@gmail.com>
2021-11-30 16:43:45 -0100

    thuesday updates3

--- lib/UI/home/components/pop_up_message.dart
+++ lib/UI/home/components/pop_up_message.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:manda_bai/Core/app_colors.dart';
 import 'package:manda_bai/Core/app_fonts.dart';
 import 'package:manda_bai/Core/app_images.dart';
+import 'package:manda_bai/UI/home/pages/start_page.dart';
 
 class Pop_up_Message extends StatefulWidget {
   const Pop_up_Message({Key? key}) : super(key: key);
@@ -18,7 +20,7 @@ class _Pop_up_MessageState extends State<Pop_up_Message> {
       body: Center(
         child: Container(
           width: Get.width,
-          height: Get.height * 0.4,
+          height: Get.height * 0.3,
           margin:
               EdgeInsets.only(left: Get.width * 0.12, right: Get.width * 0.12),
           decoration: BoxDecoration(
@@ -27,17 +29,59 @@ class _Pop_up_MessageState extends State<Pop_up_Message> {
           ),
           child: Column(
             children: [
-
-                  Icon(
-                    Icons.info,
-                    color: Colors.white,
-                    size: Get.height * 0.25,
+              Padding(
+                padding: EdgeInsets.only(
+                  top: Get.height * 0.04,
+                ),
+                child: Icon(
+                  Icons.error_outline,
+                  color: Colors.grey,
+                  size: Get.height * 0.09,
+                ),
+              ),
+              SizedBox(height: Get.height * 0.01),
+              Text(
+                'ERRO',
+                style: TextStyle(
+                    fontFamily: AppFonts.poppinsRegularFont,
+                    fontSize: Get.width * 0.05,
+                    color: Colors.grey),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Container(
+                height: Get.height * 0.07,
+                width: Get.width * 0.3,
+                decoration: BoxDecoration(
+                  color: AppColors.greenColor,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(35),
                   ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 2.0,
+                      spreadRadius: 0.0,
+                      offset: Offset(
+                          2.0, 2.0), // changes position of shadow
+                    ),
+                  ],
+                ),
 
+                child: TextButton(
+                  child: Text(
+                    'OK',
+                    style: TextStyle(
+                        fontFamily: AppFonts.poppinsBoldFont,
+                        fontSize: Get.width * 0.035,
+                        color: Colors.white),
+                  ),
+                  onPressed: () => Navigator.pop(
+                    context,
 
-
-
-        ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
