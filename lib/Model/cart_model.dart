9029cac6d96316e7dca78ebb7e66da91180583d8
rossana <mendesro03@gmail.com>
2021-12-08 11:33:02 -0100

    update Cart

--- lib/Model/cart_model.dart
+++ lib/Model/cart_model.dart
@@ -1,30 +1,58 @@
 import 'dart:ffi';
 
 class CartModel {
-  final String name, image, item_key;
-  double  price;
-  int amount;
-  int id;
-  bool checkout;
+  String? _name, _image, _item_key;
+  double? _price;
+  int? _amount;
+  int? _id;
+  bool? _checkout;
 
-  CartModel(
-      {required this.id,
-      required this.name,
-      required this.image,
-      required this.amount,
-      required this.item_key,
-       required this.checkout,
-      required this.price});
+  CartModel({
+    String? name,
+    String? image,
+    String? item_key,
+    double? price,
+    int? id,
+    int? amount,
+    bool? checkout,
+  }) {
+    _name = name;
+    _item_key = item_key;
+    _price = price;
+    _id = id;
+    _amount = amount;
+    _checkout = checkout;
+  }
+  String? get name => _name;
+  String? get image => _image;
+  String? get item_key => _item_key;
+  double? get price => _price;
+  int? get id => _id;
+  int? get amount => _amount;
+  bool? get checkout => _checkout;
 
-  factory CartModel.fromJson(Map<String, dynamic> json) {
-    return CartModel(
-      item_key: json['item_key'],
-      name: json['name'],
-      id: json['id'],
-      amount: json['quantity']['value'],
-      price: double.parse(json['price']),
-      image: json['featured_image'].toString(),
-      checkout:false,
-    );
+  set Name(String val) => _name = val;
+  set Image(String val) => _image = val;
+  set Item_key(String val) => _item_key = val;
+  set Price(double val) => _price = val;
+  set Id(int val) => _id = val;
+  set Amount(int val) => _amount = val;
+  set Checkout(bool val) => _checkout = val;
+
+  CartModel.fromJson(dynamic json) {
+    item_key:
+    json['item_key'];
+    name:
+    json['name'];
+    id:
+    json['id'];
+    amount:
+    json['quantity']['value'];
+    price:
+    double.parse(json['price']);
+    image:
+    json['featured_image'].toString();
+    checkout:
+    false;
   }
 }
