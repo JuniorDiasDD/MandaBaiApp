5771546a669c32cc7c431e65f51873af720effa3
rossana <mendesro03@gmail.com>
2021-12-08 11:08:02 -0100

    update Location

--- lib/Model/location.dart
+++ lib/Model/location.dart
@@ -1,18 +1,50 @@
 import 'dart:convert';
 
 class Location{
-  String name,island,city,endereco,phone;
-  int id;
-  Location({required this.id,required this.name,required this.island,required this.city,required this.endereco,required this.phone});
-   factory Location.fromJson(Map<String, dynamic> jsonData) {
-    return Location(
-      id: jsonData['id'],
-      name: jsonData['name'],
-      island: jsonData['island'],
-      city: jsonData['city'],
-      endereco: jsonData['endereco'],
-       phone: jsonData['phone'],
-    );
+  String? _name,_island,_city,_endereco,_phone;
+    int? _id;
+
+  Product(
+      {String? name,
+        String? island,
+        String? city,
+        String? endereco,
+        String? phone,
+        int? id,
+        }) {
+    _name = name;
+    _island = island;
+    _city = city;
+    _endereco = endereco;
+    _phone = phone;
+    _id = id;
+
+  }
+  String? get name => _name;
+  String? get island => _island;
+  String? get city => _city;
+  String? get endereco => _endereco;
+  String? get phone => _phone;
+  int? get id => _id;
+
+
+  set Name(String val) => _name = val;
+  set Island(String val) => _island = val;
+  set City(String val) => _city = val;
+  set Endereco(String val) => _endereco = val;
+  set Phone(String val) => _phone = val;
+  set Id(int val) => _id = val;
+
+
+  Location.fromJson( dynamic jsonData) {
+
+      id: jsonData['id'];
+      name: jsonData['name'];
+      island: jsonData['island'];
+      city: jsonData['city'];
+      endereco: jsonData['endereco'];
+       phone: jsonData['phone'];
+
   }
   static Map<String, dynamic> toMap(Location item) => {
         'id': item.id,
